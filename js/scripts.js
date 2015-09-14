161dc65d4425c2cd53e05fb0160403fa0d889923
Bunker & Austin Kincaid <student@epicodus.com>
2015-09-14 14:26:02 -0700

    add delete function

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,8 @@ $(document).ready(function() {
     var inputtedDescription = $("input#new-description").val();
 
     var newTasks = { name: inputtedName, description: inputtedDescription };
-
-    $("ul#tasks").append("<li><span class='tasks'>" + newTasks.name + "</span></li>");
+    //give li a unique id to select later for deletion.
+    $("ul#tasks").append("<li class='tasks task-" + newTasks.name + "'>" + newTasks.name + "</li>");
 
     $(".tasks").last().click(function() {
       $("#show-tasks").show();
@@ -16,8 +16,30 @@ $(document).ready(function() {
       $("#show-tasks h2").text(newTasks.name);
       $(".name").text(newTasks.name);
       $(".description").text(newTasks.description);
+
     });
+
+    $("input#new-name").val("");
+    $("input#new-description").val("");
+    $(".complete-task").click(function() {
+      debugger;
+      $("#completed-tasks").append("<li><span class='tasks'>" + newTasks.name + "</span></li>");
+      $("#show-tasks h2").text("");
+      $(".name").text("");
+      $(".description").text("");
+      $(".task-" + newTasks.name).remove();
+    });
+
   });
-  $("input#new-name").val("");
-  $("input#new-description").val("");
 });
+
+
+
+$(".new-address").each(function() {
+     var inputtedStreet = $(this).find("input.new-street").val();
+     var inputtedCity = $(this).find("input.new-city").val();
+     var inputtedState = $(this).find("input.new-state").val();
+
+     var newAddress = { street: inputtedStreet, city: inputtedCity, state: inputtedState };
+     newContact.addresses.push(newAddress);
+   });
